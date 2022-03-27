e1c813a4019819e2d48f32cb45590f5e98ba46a0
Lester Covey <me@aydar.media>
2022-03-27 15:52:01 +0300

    Create php.php

---
+++ php.php
@@ -0,0 +1,8 @@
+#!/usr/bin/env php
+<?php
+/*************************
+COPYRIGHT LESTER COVEY (me@lestercovey.ml)
+
+*************************/
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
