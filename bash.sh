548477d8c5d795ed29b77c9150bba6f5de0f3739
Lester Covey <me@aydar.media>
2022-04-23 12:58:16 +0300

    Added optional checks

--- bash.sh
+++ bash.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+        echo "Illegal number of arguments. Expected 'SERVICE NAME'"
+        exit
+fi
+if [[ ! "$EUID" = 0 ]]; then
+        echo "Sudo is required."
+        exit
+fi
